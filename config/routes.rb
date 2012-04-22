874554a5a22955029a507e54916865b1ed9d6fd1
Daniel Wiklund <daniel@happyrabbit.com>
2012-04-22 16:09:49 +0800

    Added route for tags

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Blog::Application.routes.draw do
     post :comment, :on => :member
   end
   get "tag/:tag" => "posts#by_tag", :as => :tag
+  get "tags" => "tags#index", :as => :tags
   get ":year/:month" => "posts#archive", :as => :archive, :constraints => { :year => /\d{4}/, :month => /\d{2}/ }
   resources :drafts, :only => [:index, :show, :update, :destroy]
 

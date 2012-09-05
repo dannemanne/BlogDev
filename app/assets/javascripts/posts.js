3a72e733060ac72b58e9ed6e5220fa4d22e9544d
Daniel Wiklund <daniel@happyrabbit.com>
2012-09-05 12:11:03 +0800

    Added preventDefault to stop page from jumping to top

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,6 @@
 $(function(){
-  $('[data-toggle]').click(function(){
+  $('[data-toggle]').click(function(evt){
+    evt.preventDefault();
     $node = $('#'+this.getAttribute('data-toggle'));
     if($node.is(':visible')){
       $node.fadeOut();

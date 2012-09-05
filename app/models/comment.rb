713a7b474e19dad7f6e19800b8d42cb3447c4bbd
Daniel Wiklund <daniel@happyrabbit.com>
2012-09-05 12:12:11 +0800

    Added ancestry and attribute accessible

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post, :counter_cache => true
 
+  has_ancestry
+
+  attr_accessible :title, :message, :parent_id
+
   #validates_existence_of :user, :post
   validates_presence_of :message, :title
   validates_length_of :message, maximum: 255

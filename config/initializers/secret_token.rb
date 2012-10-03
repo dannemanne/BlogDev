f528ddc567a4fb979c803b00768decda7414294c
Daniel Wiklund <daniel@happyrabbit.com>
2012-10-03 17:13:42 +0800

    Moved sensitive information to environment variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '864de74561a28b32f382374c18a83d8639e88eec9c6cd3d1e25f5ef2df2feb85f9b008c5780790fe31ef207c4957851f89579edeb44322dfbe4015453366d777'
+Blog::Application.config.secret_token = ENV['SECRET_TOKEN']
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Blog::Application.config.session_store :cookie_store, :key => '_Blog_session'
+Blog::Application.config.session_store :cookie_store, :key => ENV['SESSION_KEY']
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

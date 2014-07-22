a5cff4975f4c905022ac1f5d6feeb8e485a124d7
Daniel Wiklund <daniel@happyrabbit.com>
2014-07-22 09:11:19 +0800

    Splitting transaction code up into multiple methods

--- app/trans_forms/post_form.rb
+++ app/trans_forms/post_form.rb
@@ -7,10 +7,18 @@ class PostForm < ApplicationTransForm
   validates :post,          presence: true
 
   transaction do
+    save_post!
+    save_tags!
+  end
+
+private
+  def save_post!
     post.attributes = { title: title, body: body, status: status, allow_comments: allow_comments }
     post.user = current_user
     post.save!
+  end
 
+  def save_tags!
     # Parses +tag_names+ an assigns appropriate Tags
     post.tags = tag_names.split(',').map { |tag| Tag.find_or_create_by(name: tag.strip) }
     post.save!

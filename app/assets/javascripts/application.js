111bce92974b3f480951bb7afe40fa31042dd99e
Daniel Wiklund <daniel@happyrabbit.com>
2019-12-06 17:54:00 +0800

    Fix js function reference

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,7 @@
 // changed, we'll call Showdown to convert the textarea
 // input to HTML then prettify to colorize code blocks.
 var prev_text = "";
-function parse_live(node, preview) {
+window.parse_live = function(node, preview) {
     var input_text = node.val();
     if (input_text !== prev_text) {
         var showdown = new Showdown.converter();
@@ -16,22 +16,22 @@ function parse_live(node, preview) {
         Prism.highlightAll()
         prev_text = input_text;
     }
-}
-function parse_once(node){
+};
+window.parse_once = function(node){
     var showdown = new Showdown.converter();
     node.html(showdown.makeHtml(node.text()));
     Prism.highlightAll()
-}
+};
 
 // Setup the WYSIWYM editor!
 jQuery(window).load(function() {
   jQuery("[data-parse]").each(function(i, node){
     var $node = jQuery(node);
     if($node.attr("data-parse") === "once"){
-      parse_once($node);
+      window.parse_once($node);
     }else if($node.attr("data-parse") === "live"){
       setInterval(function(){
-        parse_live($node, jQuery($node.attr("data-parse-preview")));
+        window.parse_live($node, jQuery($node.attr("data-parse-preview")));
       }, 200);
     }
   });

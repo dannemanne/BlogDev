435af49af55314085721e88ecceec46f8b7d5d9e
Daniel Wiklund <daniel@happyrabbit.com>
2020-09-15 23:07:40 +0800

    Precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
+  config.assets.digest = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.digest = true
 end

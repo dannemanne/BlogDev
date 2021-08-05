fde37b8a2a32ce1ec5ce2e1b27fc505c680eb5c2
Daniel Wiklund <daniel@happyrabbit.com>
2021-08-05 11:21:03 +0800

    Confirm webpacker:compile is working

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,1 +1,9 @@
-import "../stylesheets/application.scss";
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+//import "channels"
+import "stylesheets/application.scss"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()

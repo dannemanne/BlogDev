7d2a90cee21e3fe6fbe297e829747e9d08919cc7
Daniel Wiklund <daniel@happyrabbit.com>
2021-08-05 14:00:42 +0800

    Remove turbolinks

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,9 +1,9 @@
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+//import Turbolinks from "turbolinks"
+//import * as ActiveStorage from "@rails/activestorage"
 //import "channels"
 import "stylesheets/application.scss"
 
 Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+//Turbolinks.start()
+//ActiveStorage.start()
